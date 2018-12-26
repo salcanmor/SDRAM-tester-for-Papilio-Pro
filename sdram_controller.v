9feff0c1d047cf4c9933ec1810f7a70efa04c818
Salvador Canas Moreno <cybero@hotmail.es>
2018-12-26 02:27:09 +0100

    Update sdram_controller.v

--- sdram_controller.v
+++ sdram_controller.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-module sdram_controller2( sdram_cke, sdram_clk, sdram_cs_n, sdram_we_n, sdram_ras_n, sdram_cas_n, sdram_a, sdram_ba, sdram_udqm, sdram_ldqm, sdram_dq, sys_clk, sys_reset, sys_addr, sys_cmd, sys_data_in, sys_data_out);
+module sdram_controller2( sdram_cke, sdram_clk, sdram_cs_n, sdram_we_n, sdram_ras_n, sdram_cas_n, sdram_addr, sdram_ba, sdram_dqmh_n, sdram_dqml_n, sdram_dq, sys_clk, sys_reset, sys_addr, sys_cmd, sys_data_in, sys_data_out);
 
   // sdram interface
   output wire sdram_cke;			//	 Clock enable
@@ -21,7 +21,7 @@ module sdram_controller2( sdram_cke, sdram_clk, sdram_cs_n, sdram_we_n, sdram_ra
   output wire sdram_dqmh_n;      // mascara para byte alto o bajo
   output wire sdram_dqml_n;      // durante operaciones de escritura
 
-  inout tri [15:0] sdram_dq
+  inout tri [15:0] sdram_dq;
 
   // host interface
 
@@ -29,12 +29,14 @@ module sdram_controller2( sdram_cke, sdram_clk, sdram_cs_n, sdram_we_n, sdram_ra
   input wire sys_reset;              // normalmente conectado a la versión negada del pin "locked" del PLL/MMCM
 
 
-  input wire [22:0] sys_addr,        // address to SDRAM (up to 16M addresses)
+  input wire [22:0] sys_addr;        // address to SDRAM (up to 16M addresses)
 
-  input wire [22:0] sys_cmd,        // address to SDRAM (up to 16M addresses)
-  input wire [15:0] sys_data_in,        // address to SDRAM (up to 16M addresses)
-  input wire [15:0] sys_data_out,        // address to SDRAM (up to 16M addresses)
+  input wire [22:0] sys_cmd;        // address to SDRAM (up to 16M addresses)
+  input wire [15:0] sys_data_in;        // address to SDRAM (up to 16M addresses)
+  input wire [15:0] sys_data_out;        // address to SDRAM (up to 16M addresses)
+  
+  
+  
+  
 
 endmodule
-
-
